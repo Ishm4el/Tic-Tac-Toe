3dd12e5bd3ae1513a4c40b1bc3a93a1b4c52cae0
Ishii <ismaelv@protonmail.com>
2023-11-09 04:10:45 -0800

    Completed the Tic-tac-toe game. + Players can write their names + Game will mark who has won + Game will not allow players to replace already placed zones + Reset Button to reset the board + Game will not allow players to continue placing markers if a player has already won.

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-//Board
-// 0 1 2
-// 3 4 5
-// 6 7 8
 const game = (function () {
-    
+    const players = { player1 : "Player 1", player2 : "Player 2"};
+
+    function boot(player1, player2) {
+        if (player1 !== '')
+            players.player1 = player1;
+        if (player2 !== '')
+            players.player2 = player2;
+        
+        doc.sidebarPlayer.innerHTML = players.player1;
+        run();
+    }
+
     const x = 1;
     const o = 0;
     const board = new Array(9);
 
-    // Determine whether X or O has won; if so, end;
-    // else, continue.
+    // Determine whether X or O has won; if so, end; else, continue.
     function determineVictory() {
-        if (checkForThree(x)) {
-            return !true;
-        }
-        else if (checkForThree(o)) {
-            return !true;
-        }
-        return !false;
+        return checkForThree(x) ? true : checkForThree(o) ? true : false;
     }
 
     // The validator for checking if a player has won.
@@ -29,27 +29,71 @@ const game = (function () {
             board[1] === marker && board[4] === marker && board[7] === marker || // vertical mid
             board[2] === marker && board[5] === marker && board[8] === marker || // vertical rigth
             board[0] === marker && board[4] === marker && board[8] === marker || // diagnal left top to right bot
-            board[6] === marker && board[4] === marker && board[2] === marker) { // diagnal left bot to right top
-                return true;
-            }
+            board[6] === marker && board[4] === marker && board[2] === marker)   // diagnal left bot to right top 
+            return true;
         return false;
     }
 
     // The main game
     function run() {
         let playerTurn = x; // general rule is that X goes first.
-        const plots = document.querySelectorAll("#game > div");
-        plots.forEach((element) => {
-            element.addEventListener('click', () => {
-                element.innerHTML = playerTurn === x ? 'X' : 'O';
-                board[element.dataset] = playerTurn;
-                playerTurn = playerTurn === x ? o : x;
-                console.log(playerTurn);
-            });
+        doc.playerMarker.innerHTML = 'X';
+        function boardReaction(element) {
+            element.target.removeEventListener('click', boardReaction)
+            if (playerTurn === x) {
+                element.target.innerHTML = 'X';
+                board[parseInt(element.target.dataset.position)] = playerTurn;
+                playerTurn = o;
+                doc.playerMarker.innerHTML = "O";
+                doc.sidebarPlayer.innerHTML = players.player2;
+            }
+            else {
+                element.target.innerHTML = 'O';
+                board[parseInt(element.target.dataset.position)] = playerTurn;
+                playerTurn = x;
+                doc.playerMarker.innerHTML = "X";
+                doc.sidebarPlayer.innerHTML = players.player1;
+            }
+            if (determineVictory()) {
+                doc.plots.forEach((element) => {
+                    element.removeEventListener('click', boardReaction);
+                    doc.playerMarker.innerHTML = playerTurn === x ? "O" : "X";
+                    doc.playerMarker.classList.toggle("victor");
+                });
+            }
+        }
+
+        doc.plots.forEach((element) => {
+            element.addEventListener('click', boardReaction);
         });
     }
-    return {run, determineVictory};
+    function resetGame() {
+        doc.plots.forEach((element) => {
+            element.innerHTML = '';
+        });
+        for (let i = 0; i < board.length; i++) { board[i] = undefined; }
+        doc.playerMarker.classList.toggle("victor");
+        run();
+    }
+    return {boot, resetGame};
 })();
 
-const displayMarker = document.querySelector("#currentMarker");
-console.log(game.run()); // run the game.
+const doc = (function () {
+    const plots = document.querySelectorAll("#game > div");
+    const playerMarker = document.querySelector(".currentMarkerSpan");
+    const form = document.querySelector("form");
+    const dialog = document.querySelector("#createPlayer");
+    const submitButton = document.querySelector("#startGame");
+    const resetButton = document.querySelector("#resetButton");
+    const sidebarPlayer = document.querySelector("#currentPlayer");
+    return { plots, playerMarker, form, dialog, submitButton, resetButton, sidebarPlayer };
+})();
+
+doc.resetButton.addEventListener('click', () => {
+    game.resetGame();
+});
+doc.submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    game.boot(doc.form.elements["playerOne"].value, doc.form.elements["playerTwo"].value);
+    doc.dialog.style.display = 'none';
+});
